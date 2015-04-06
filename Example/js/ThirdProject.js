00d270d394317cc68074091d78bf2e2d3a34cd8c
kocyigityunus <kocyigityunus@gmail.com>
2015-04-06 20:23:40 +0300

    added example

---
+++ Example/js/ThirdProject.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var MainNavigator = require('./MainNavigator/MainNavigator');
+
+var ThirdProject = React.createClass({
+  render: function() {
+    return (
+      <MainNavigator />
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ThirdProject', () => ThirdProject);
