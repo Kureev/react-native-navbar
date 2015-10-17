d58ea31c0039f8f14c161a4707a853eedef6269f
Kureev Alexey <kureev-mail@ya.ru>
2015-10-18 01:02:31 +0200

    Update Basic example

--- examples/Basic/index.ios.js
+++ examples/Basic/index.ios.js
@@ -1,53 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
-var React = require('react-native');
-var {
+import React from 'react-native';
+const {
   AppRegistry,
-  StyleSheet,
+  Component,
   Text,
-  View,
+  View
 } = React;
+import NavigationBar from 'react-native-navbar';
 
-var Basic = React.createClass({
-  render: function() {
+class Basic extends Component {
+  render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={{ flex: 1, backgroundColor: '#ff9900' }}>
+        <NavigationBar
+          title={{ title: 'Title', }}
+          leftButton={{ title: 'Back', }}
+          rightButton={{ title: 'Forward', }} />
       </View>
     );
   }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+}
 
 AppRegistry.registerComponent('Basic', () => Basic);
