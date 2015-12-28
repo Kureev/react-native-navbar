f46e5de9c44211e88a0532f4a6714462272af63e
Kureev Alexey <kureev-mail@ya.ru>
2015-12-28 21:05:21 +0100

    Migrate Basic example to 0.17

--- examples/Basic/index.ios.js
+++ examples/Basic/index.ios.js
@@ -3,14 +3,14 @@ const {
   AppRegistry,
   Component,
   Text,
-  View
+  View,
 } = React;
-import NavigationBar from 'react-native-navbar/ios';
+import NavigationBar from 'react-native-navbar';
 
 class Basic extends Component {
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: '#ff9900' }}>
+      <View style={{ flex: 1, backgroundColor: '#ff9900', }}>
         <NavigationBar
           title={{ title: 'Title', }}
           leftButton={{ title: 'Back', }}
