c054cf8267ec2bbd76d6934250644010f6cf1e17
Kureev Alexey <kureev-mail@ya.ru>
2016-03-17 23:48:40 +0100

    Remove legacy animation flag for StatusBar

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const StatusBarShape = {
 function customizeStatusBar(data) {
   if (Platform.OS === 'ios') {
     if (data.style) {
-      StatusBar.barStyle(data.style, true);
+      StatusBar.barStyle(data.style);
     }
     const animation = data.hidden ?
     (data.hideAnimation || NavigationBar.defaultProps.statusBar.hideAnimation) :
