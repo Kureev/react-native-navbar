e289bf0a376c7f611e985acf8ba160979ed388f7
Xing Zheng <betree.x@gmail.com>
2016-03-21 14:02:36 +0800

    Use flex box style to make buttons and title align center in vertical, instead of using fixed margin or padding.

--- styles.js
+++ styles.js
@@ -13,6 +13,7 @@ module.exports = {
     height: NAV_BAR_HEIGHT,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'stretch',
   },
   customTitle: {
     position: 'absolute',
@@ -22,22 +23,26 @@ module.exports = {
     alignItems: 'center',
   },
   navBarButton: {
-    marginTop: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   navBarButtonText: {
     fontSize: 17,
     letterSpacing: 0.5,
-    marginTop: 12,
+  },
+  navBarTitleContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   navBarTitleText: {
     fontSize: 17,
     letterSpacing: 0.5,
     color: '#333',
     fontWeight: '500',
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: 7,
-    textAlign: 'center',
   },
 };
