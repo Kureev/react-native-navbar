b61c0e4fde0f985d0d27596de58f212c5626fe5f
Kureev Alexey <kureev-mail@ya.ru>
2016-10-12 17:11:53 +0200

    Update Basic example to use modern react-native version

--- examples/Basic/index.ios.js
+++ examples/Basic/index.ios.js
@@ -1,10 +1,9 @@
-import React from 'react-native';
-const {
+import React, { Component } from 'react';
+import {
   AppRegistry,
-  Component,
   Text,
   View,
-} = React;
+} from 'react-native';
 import NavigationBar from 'react-native-navbar';
 
 class Basic extends Component {
